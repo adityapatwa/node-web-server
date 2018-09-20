dce4d093900a8d22fc37ae1fe0712826a2a27f21
Aditya Patwa <adityapatwa@live.com>
2018-09-20 14:51:22 -0400

    Setup the basic express app and added the handlebar views and partials. Also setup some helper methods to fetch the current year and convert string into uppercase

--- server.js
+++ server.js
@@ -1,17 +1,38 @@
 const express = require('express');
+const hbs = require('hbs');
 
 let app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+
 app.get('/', (req, res) => {
     // res.send('<h1>Hello Express!</h1>');
-    res.send({
-        name: 'Aditya',
-        likes: ['Video Games', 'Football']
+    // res.send({
+    //     name: 'Aditya',
+    //     likes: ['Video Games', 'Football']
+    // });
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to the home page of my website',
     });
 });
 
 app.get('/about', (req, res) => {
-    res.send('<h1>About Page</h1>');
+    // res.send('<h1>About Page</h1>');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
 });
 
 app.get('/bad', (req, res) => {
@@ -20,5 +41,7 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is up on Port 3000');
+});
 

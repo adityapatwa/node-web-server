cc9dabe637798bcec937d46da56e2ebe7cfd30c0
Aditya Patwa <adityapatwa@live.com>
2018-09-20 15:11:10 -0400

    Refactored the code in the partials

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const hbs = require('hbs');
 
 let app = express();
@@ -6,6 +7,23 @@ let app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
